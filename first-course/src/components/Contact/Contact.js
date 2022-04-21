9b859f310a8ba3d0c0ccea500af8812a1a489074
samuelalonsogil <asbarxas.samuelalonso@gmail.com>
2022-04-21 23:06:02 +0200

    props

--- first-course/src/components/Contact/Contact.js
+++ first-course/src/components/Contact/Contact.js
@@ -2,28 +2,28 @@ import profilePicture from './maxresdefault.jpg';
 import mailIcon from './mail-icon.png';
 import phoneIcon from './phone.png';
 
-export default function Contact(){
+export default function Contact(props){
     return (
         <>
         <div id={'contact-wall'}>
             <div id={'contact-main-container'}>
                 <div id={'container-profile-picture-contact'}>
-                    <img id={'profile-picture-contact'} src={profilePicture} alt={'profile picture'}/>
+                    <img id={'profile-picture-contact'} src={props.img} alt={'profile picture'}/>
                 </div>
                 <div id={'info-contact-container'}>
                     <div id={'main-info-contact'}>
-                        <p id={'name-contact'}>Botitas</p>
+                        <p id={'name-contact'}>{props.name}</p>
                     </div>
                     <div id={'details-info-contact'}>
 
                         <div>
                             <img src={phoneIcon} alt={'phone icon'} width={12}/>
-                            <span className={'icons-contact'} >(+34) 696550102</span>
+                            <span className={'icons-contact'} >(+34) {props.phone}</span>
                         </div>
 
                         <div>
                             <img src={mailIcon} alt={'email icon'} width={15}/>
-                            <span className={'icons-contact'} >asbarxas.samuelalonso@gmail.com</span>
+                            <span className={'icons-contact'} >{props.email}</span>
                         </div>
                     </div>
                 </div>
--- first-course/src/components/Contact/ContactApp.js
+++ first-course/src/components/Contact/ContactApp.js
@@ -1,14 +1,38 @@
 import Contact from "./Contact";
+import image01 from './maxresdefault.jpg';
+import image02 from './ben.jpg';
+import image03 from './gojo.PNG';
+import image04 from './zoro.jpg';
 
 export default function ContactApp() {
     return (
         <>
             <div id={'container-main-contact-app'}>
-                <Contact className={'element'}/>
-                <Contact className={'element'}/>
-                <Contact className={'element'}/>
-                <Contact className={'element'}/>
-                <Contact className={'element'}/>
+                <Contact 
+                    img = {image01}
+                    name = 'Botitas'
+                    phone = '676890453'
+                    email = 'botitas@gail.com'
+                />
+                <Contact
+                    img = {image02}
+                    name = 'Gato'
+                    phone = '676890453'
+                    email = 'botitas@gail.com'
+                />
+                <Contact
+                    img = {image03}
+                    name = 'Perro'
+                    phone = '676890453'
+                    email = 'botitas@gail.com'
+                />
+                <Contact
+                    img = {image04}
+                    name = 'Ordfenador'
+                    phone = '676890453'
+                    email = 'botitas@gail.com'
+                />
+
             </div>
         </>
     )
